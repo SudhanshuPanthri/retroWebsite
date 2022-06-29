2c71dfdfea2f17c51b44e763dd4b9c54ef2f7bc9
sudhanshu panthri <panthrisudhanshu666@gmail.com>
2022-06-29 11:54:07 +0530

    name screen

--- src/screens/Greeting.jsx
+++ src/screens/Greeting.jsx
@@ -1,6 +1,15 @@
 import React, { useEffect } from 'react'
 import '../css/Greeting.css'
 import { useNavigate } from 'react-router-dom'
+import { motion } from 'framer-motion'
+
+const bounceTransition = {
+    y: {
+        duration: 0.4,
+        yoyo: Infinity,
+        ease: "easeInOut",
+    },
+}
 const Greeting = () => {
     const navigate = useNavigate()
     const keyPressed = () => {
@@ -11,10 +20,15 @@ const Greeting = () => {
     }, [])
     return (
         <div className='greetingParent'>
-            <div className='paraContainer'>
+            <motion.div className='paraContainer'
+                animate={{
+                    y: ["10%", "0%"],
+                }}
+                transition={bounceTransition}
+            >
                 <p className='para'>Press Space to enter the world</p>
-            </div>
-        </div>
+            </motion.div>
+        </div >
     )
 }
 
--- src/screens/Name.jsx
+++ src/screens/Name.jsx
@@ -1,11 +1,74 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { motion } from 'framer-motion'
 import '../css/Name.css'
 const Name = () => {
+    // const [keyPressed, setKeyPressed] = useState(true)
+    const line1 = "Hi, I'm Sudhanshu Panthri....."
+    const line2 = "Mind If I Take You On A Journey ?"
+    const line3 = `Press --> Arrow key to move`
+
+    const sentence = {
+        hidden: { opacity: 1 },
+        visible: {
+            opacity: 1,
+            transition: {
+                delay: 1.25,
+                staggerChildren: 0.08,
+            },
+        }
+    }
+
+    const letter = {
+        hidden: { opacity: 0, y: 50 },
+        visible: { opacity: 1, y: 0 }
+    }
+
+    // const down = () => {
+    //     if (keyPressed) {
+    //         setKeyPressed(!keyPressed)
+    //     }
+    //     else {
+    //         setKeyPressed(!keyPressed)
+    //     }
+    // }
+    // useEffect(() => {
+    //     window.addEventListener('keydown', down)
+    //     return (
+    //         window.removeEventListener('keydown', down)
+    //     )
+    // }, [])
     return (
-        <div className='nameParent'>
-            <div className='itachi'></div>
-            <p>Hi i'm sudhanshu panthri</p>
-        </div>
+        <motion.div className='nameParent'
+        >
+            <motion.div className='itachi'></motion.div>
+            <motion.div className='greetingMessage'
+                variants={sentence}
+                initial="hidden"
+                animate="visible">
+                {line1.split("").map((char, index) => {
+                    return (
+                        <motion.span key={char + "-" + index} variants={letter}>
+                            {char}
+                        </motion.span>
+                    )
+                })}
+                <br />
+                {line2.split("").map((char, index) => {
+                    return (
+                        <motion.span key={char + "-" + index} variants={letter}>{char}</motion.span>
+                    )
+                })}
+                <br />
+                <br />
+                <br />
+                <br />
+                {line3.split("").map((char, index) => {
+                    return (
+                        <motion.span key={char + "-" + index} variants={letter}>{char}</motion.span>
+                    )
+                })}
+            </motion.div>
+        </motion.div>
 
     )
 }
